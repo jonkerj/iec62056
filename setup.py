207180840a6d4a6f550a68e47af528a9d17c6724
Jorik Jonker <jorik@kippendief.biz>
2019-04-13 19:26:58 +0200

    Improve packaging
    * get version from git tag
    * package requirements, so install does not fail

--- setup.py
+++ setup.py
@@ -1,26 +1,34 @@
 from setuptools import setup, find_packages
 
 long_description = open('README.md', 'r').read()
-reqs = [line.strip() for line in open('requirements.txt', 'r') if line and not line.startswith('#')]
+with open('requirements.txt') as f:
+	reqs = f.read().splitlines()
 
 setup(
-	name='iec62056',
-	description='Library to parse IEC62056/IEC1107/DSMR telegrams',
 	author='Jorik Jonker',
 	author_email='jorik@kippendief.biz',
-	url='https://github.com/jonkerj/iec62056',
-	version='0.1.7',
-	packages=find_packages(),
-	package_data={
-		'iec62056': ['*.lark'],
-	},
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: Software Development :: Libraries',
+	],
+	description='Library to parse IEC62056/IEC1107/DSMR telegrams',
+	include_package_data=True,
 	install_requires=reqs,
 	long_description=long_description,
 	long_description_content_type="text/markdown",
-	classifiers=[
-		'Programming Language :: Python :: 3',
-		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-		'Operating System :: OS Independent',
-		'Development Status :: 3 - Alpha',
-	],
+	name='iec62056',
+	packages=find_packages(),
+	setup_requires=['better-setuptools-git-version'],
+	url='https://github.com/jonkerj/iec62056',
+	version_config={
+		'version_format': '{tag}.dev{sha}',
+		'starting_version': '0.1.0',
+	},
 )
